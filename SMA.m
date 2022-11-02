8834af6011ca375b9b4304bdb4f542a505898612
Shihong Yin <73772224+Shihong-Yin@users.noreply.github.com>
2022-11-02 23:08:45 +0800

    Add files via upload

---
+++ SMA.m
@@ -0,0 +1,60 @@
+function [Destination_fitness,bestPositions,Convergence_curve] = SMA(N,Max_iter,lb,ub,dim,func_num)
+% Max_iter: Maximum iterations, N: Populatoin size, Convergence_curve: Convergence curve
+rand('seed',sum(100 * clock)); % Random number seed
+z = 0.03; % Adjustable parameter
+% Initialize position of slime mould
+lb = ones(1,dim).*lb; % Lower boundary 
+ub = ones(1,dim).*ub; % Upper boundary
+X = initialization(N,dim,ub,lb); % It can be downloaded from https://github.com/Shihong-Yin
+bestPositions = zeros(1,dim); % Optimal food source location
+Destination_fitness = inf; % Change this to -inf for maximization problems
+weight = ones(N,dim); % Fitness weight of each slime mould
+Convergence_curve = zeros(1,Max_iter);
+% Main loop
+for it = 1:Max_iter
+    % Check the boundary and calculate the fitness
+    FU = X>ub;  FL = X<lb;  X = (X.*(~(FU+FL)))+ub.*FU+lb.*FL;
+    PopFitness = cec21_bias_shift_rot_func(X',func_num)'; % https://github.com/Shihong-Yin
+    % Sort the fitness thus update the bF and wF
+    [SmellOrder,SmellIndex] = sort(PopFitness); % Eq.(2.6)
+    bestFitness = SmellOrder(1);
+    worstFitness = SmellOrder(N);
+    S = bestFitness-worstFitness+eps; % Plus eps to avoid denominator zero
+    % Calculate the fitness weight of each slime mould
+    for i = 1:N
+        if i <= N/2 % Eq.(2.5)
+            weight(SmellIndex(i),:) = 1+rand(1,dim)*log10((bestFitness-SmellOrder(i))/S+1);
+        else
+            weight(SmellIndex(i),:) = 1-rand(1,dim)*log10((bestFitness-SmellOrder(i))/S+1);
+        end
+    end
+    % Update the best position and destination fitness
+    if bestFitness < Destination_fitness
+        bestPositions = X(SmellIndex(1),:);
+        Destination_fitness = bestFitness;
+    end
+    a = atanh(-(it/Max_iter)+1); % Eq.(2.4)
+    vb = unifrnd(-a,a,N,dim); % Eq.(2.3)
+    b = 1-it/Max_iter;
+    vc = unifrnd(-b,b,N,dim);
+    p = tanh(abs(PopFitness-Destination_fitness)); % Eq.(2.2)
+    r = rand(N,dim);
+    A = randi([1,N],N,dim); % Two positions randomly selected from population
+    B = randi([1,N],N,dim);
+    % Update the Position of search agents
+    for i = 1:N
+        if rand < z % Eq.(2.7)
+            X(i,:) = (ub-lb).*rand(1,dim)+lb; % The original code is (ub-lb)*rand+lb;
+        else
+            for j = 1:dim
+                if r(i,j) < p(i) % Eq.(2.1)
+                    X(i,j) = bestPositions(j)+vb(i,j)*(weight(i,j)*X(A(i,j),j)-X(B(i,j),j));
+                else
+                    X(i,j) = vc(i,j)*X(i,j);
+                end
+            end
+        end
+    end
+    Convergence_curve(it) = Destination_fitness;
+end
+end
---
+++ main_SMA.m
@@ -0,0 +1,28 @@
+clear;clc
+close all
+tic
+func_num = 1; % Function number 1:10
+Algorithm_name = 'SMA';
+lb = -100;  ub = 100;  dim = 20;
+Pop_size = 100; % Number of search agents
+Max_iter = 10000; % Maximum number of iterations
+optimum = [100,1100,700,1900,1700,1600,2100,2200,2400,2500]; % The optimal value of the function
+runs = 30; % Number of independent runs
+for i = 1:runs
+    [Destination_fitness,bestPositions,Convergence_curve] = SMA(Pop_size,Max_iter,lb,ub,dim,func_num);
+    fitness(i,:) = Destination_fitness;
+    Positions(i,:) = bestPositions;
+    curve(i,:) = Convergence_curve;
+end
+Time = toc; % Record the execution time of the algorithm
+fitness = fitness-optimum(func_num);
+[~,Index] = sort(fitness);
+bestPosition = Positions(Index(1),:);
+curve = curve-optimum(func_num);
+curve(curve<1e-8) = 0;
+Convergence_curve = sum(curve,1)/runs;
+Convergence_curve = Convergence_curve(1,1:10:end);
+% Display solution results
+disp([Algorithm_name,': function ',num2str(func_num)])
+disp(['Mean fitness: ',num2str(Convergence_curve(end))])
+disp(['Execution time: ',num2str(Time)])
